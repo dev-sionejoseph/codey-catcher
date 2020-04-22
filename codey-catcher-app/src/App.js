1c05c107da9a4bca63ddedf1beebe20879fe94dd
Student_M9 <Student_M9@ps-dal-m9.home>
2020-04-22 12:51:24 -0500

    Pre-importing components

--- codey-catcher-app/src/App.js
+++ codey-catcher-app/src/App.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar'
+import Catcher from './components/Catcher'
+import LifeAnswer from './components/LifeAnswer'
+import NameInput from './components/NameInput'
+import QuestionInput from './components/QuestionInput'
 
 function App() {
   return (
