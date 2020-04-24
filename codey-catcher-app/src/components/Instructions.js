02b37767fbaa9886cc4a3802a05ccbd14b8ad1cf
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-24 11:28:21 -0500

    Instructions basics

---
+++ codey-catcher-app/src/components/Instructions.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Instructions() {
+    return (
+        <div className="instructions-div">
+            <h1>Welcome to Codey Catcher</h1>
+            <p>For any future, bright or bleak, Codey Catcher will find the answers you seek.</p> 
+                <div id="inner-instructions-wrap">
+                    <h3>Instructions</h3>
+                    <ul>
+                        <label>Divinations: </label>
+                        <li>Codey will predict who you will marry, 
+                            where you will live, how many kids or pets, and your means of transportation
+                        </li>
+                        <label>Ask Codey: </label>
+                        <li> Have a yes or no question? Ask Codey and she'll tell you what's what.
+                        </li>
+                        <label>Love or Loss: </label>
+                        <li> Want to test the match between you and your beloved? 
+                            Enter both your names in the input fields provided and Codey will calculate how likely it is to succeed.
+                        </li>
+
+                    </ul>
+                </div>
+        </div>
+    )
+}
