5b33506ff911338718d58227ded47aeeabf79b8e
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-24 09:25:30 -0500

    updated NameInput basics

---
+++ codey-catcher-app/src/components/NameInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NameInput() {
+    return (
+        <div>
+            <input type="text" id="name-one"></input>
+            <input type="text" id="name-two"></input>
+            <button id="compatibility-button">Love or Loss?</button>
+        </div>
+    )
+}
