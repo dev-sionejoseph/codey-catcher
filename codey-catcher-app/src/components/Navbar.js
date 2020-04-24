29370e345019ab3defb9b4cd056529a9e2d5a050
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-24 14:59:28 -0500

    Navbar update

--- codey-catcher-app/src/components/Navbar.js
+++ codey-catcher-app/src/components/Navbar.js
@@ -6,18 +6,18 @@ export default function NavBar() {
     return (
         <div id="navWrap">
             <nav>
+                <Link to="/">
+                    <li className="navItem" id="home-nav">Home</li>
+                </Link>
                 <Link to="/divination">
-                    <li className="navItem" id="homeNav"></li>
+                    <li className="navItem" id="divination-nav">Divination</li>
                 </Link>
                 <Link to="/askcodey">
-                    <li className="navItem" id="profileNav"></li>
+                    <li className="navItem" id="ask-codey-nav">Ask Codey</li>
                 </Link>
                 <Link to="/loveorloss">
-                    <li className="navItem" id="favoritesNav"></li>
+                    <li className="navItem" id="love-or-loss-nav">Love or Loss</li>
                 </Link>
-               
-                
-                
             </nav>
         </div>
     )
