d31cb7b7f860e03b16b5dbcf88b0795f15c3fd36
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-24 09:46:16 -0500

    more updates to NameInput

--- codey-catcher-app/src/components/NameInput.js
+++ codey-catcher-app/src/components/NameInput.js
@@ -10,14 +10,25 @@ export default class NameInput extends Component {
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
+    }
+    handleChange(e){
+        this.setState ({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    handleSubmit(e){
+        
     }
 
 
     render() {
         return (
             <div>
-                <input type="text" id="name-one" name="nameOne"></input>
-                <input type="text" id="name-two" name="nameTwo"></input>
+                <input type="text" id="name-one" name="nameOne" value={this.state.nameOne}></input>
+                <input type="text" id="name-two" name="nameTwo" value={this.state.nameTwo}></input>
                 <button id="compatibility-button">Love or Loss?</button>
             </div>
         )
