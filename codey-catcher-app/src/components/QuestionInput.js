3060149ad5d210db8ced3035884034827609d21c
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-24 14:58:30 -0500

    QuestionInput

---
+++ codey-catcher-app/src/components/QuestionInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class QuestionInput extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
