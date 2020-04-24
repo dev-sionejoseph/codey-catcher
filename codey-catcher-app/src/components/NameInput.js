5276c0df9747abc9db1d8b9bf1c7ccd4b1ceecfb
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-24 09:33:15 -0500

    updated NameInput

--- codey-catcher-app/src/components/NameInput.js
+++ codey-catcher-app/src/components/NameInput.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export default class NameInput extends Component {
+    constructor(props){
+        super(props);
+
+        this.state= {
+            nameOne:'',
+            nameTwo:''
+        }
+
+        this.handleChange = this.handleChange
+    }
+
+
     render() {
         return (
             <div>
-                <input type="text" id="name-one"></input>
-                <input type="text" id="name-two"></input>
+                <input type="text" id="name-one" name="nameOne"></input>
+                <input type="text" id="name-two" name="nameTwo"></input>
                 <button id="compatibility-button">Love or Loss?</button>
             </div>
         )
