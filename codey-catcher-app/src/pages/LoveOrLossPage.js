559205496b1699ae2da14d68e3ac20408b910414
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-25 06:48:44 -0500

    updated LoveOrLossPage

---
+++ codey-catcher-app/src/pages/LoveOrLossPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class LoveOrLossPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            
+        }
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
