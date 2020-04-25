5471a115188c5057188b58ab1e461be60acda377
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-25 05:15:59 -0500

    updates to SpouseGender

--- codey-catcher-app/src/components/SpouseGenderSelect.js
+++ codey-catcher-app/src/components/SpouseGenderSelect.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-export default class SpouseGenderSelect extends Component {
+export default class SpouseGenderSelect extends Component{
     render() {
         return (
             <div id="spouse-gender-wrap">
                 <label>Spouse Gender?</label>
-                <select id="gender-select" key="chosenGender" onChange={this.props.onChange}>
-                    <option value="none" ></option>
-                    <option value="man" >Man</option>
+                <select id="gender-select" name={this.props.name} onChange={this.props.onChange}>
+                    <option value="none"></option>
+                    <option value="man">Man</option>
                     <option value="woman">Woman</option>
                     <option value="nonbinary">Nonbinary</option>
                 </select>
