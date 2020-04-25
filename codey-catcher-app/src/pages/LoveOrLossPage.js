2aac252bc8412252e527b7684b49155209d81d9b
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-25 16:16:58 -0500

    updated LoveOrLoss with axios

--- codey-catcher-app/src/pages/LoveOrLossPage.js
+++ codey-catcher-app/src/pages/LoveOrLossPage.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import NameInput from '../components/NameInput';
+import Catcher from '../components/Catcher';
 
 export default class LoveOrLossPage extends Component {
     constructor(props) {
@@ -9,19 +12,46 @@ export default class LoveOrLossPage extends Component {
             sname:'',
 
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(e){
+        this.setState ({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit(e){
+        axios.get('https://httpbin.org/get', {
+            headers: {
+                "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+		        "x-rapidapi-key": "fca71f8c8cmshdc95ba1e09ee3efp1fcc75jsn871d103cc45b"
+            },
+        })
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => {
+            this.setState({
+                result: "uh oh. something went wrong!"
+            })
+        });
+    }
+
     render() {
         return (
             <div id="love-or-loss-page">
                 <div id="love-loss-left">
-
+                    <NameInput onChange={this.handleChange} onClick={this.handleSubmit} />
                 </div>
                 <div id="love-loss-center">
 
                 </div>
                 <div id="love-loss-right">
                     <div id="love-loss-results">
-                        
+
                     </div>
                 </div>
                 
