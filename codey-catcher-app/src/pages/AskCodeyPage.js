adf65f98e78c01d56bd6e47e96a36971c723d240
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-26 00:49:35 -0500

    AskCodey update

--- codey-catcher-app/src/pages/AskCodeyPage.js
+++ codey-catcher-app/src/pages/AskCodeyPage.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Catcher from '../components/Catcher'
 import QuestionInput from '../components/QuestionInput'
 
 
-let answers = ["The stars say yes.", "The vision is unclear; proceed with caution.", "The answer is no"]
-let currentAnswer = answers[]
 
-export default function AskCodeyPage() {
-    return (
-        <div>
-            <div id="askcodey-top">
-                <QuestionInput />
+export default class AskCodeyPage extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            answerArray:["The stars say yes.", "The vision is unclear; proceed with caution.", "The answer is no"],
+            currentAnswer:""
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick(){
+        
+        let answers= this.state.answerArray
+        
+        this.setState({
+         currentAnswer: answers[Math.floor(Math.random() * answers.length)]
+        }) 
+    }
+
+
+    render(){
+        return (
+            <div>
+                <div id="askcodey-top">
+                    <QuestionInput onClick={this.handleClick}/>
+                </div>
+                <div id="askcodey-center">
+                    <Catcher />
+                </div>
+                <div id="askcodey-bottom">
+                    <p id="askcodey-result"> {this.state.currentAnswer} </p>
+                </div>
             </div>
-            <div id="askcodey-center">
-                <Catcher />
-            </div>
-            <div id="askcodey-bottom">
-                
-            </div>
-        </div>
-    )
+        )
+    }
 }
