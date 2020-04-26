8b1d536eb1f7beecc297eceb8e589dc36801c179
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-25 20:36:00 -0500

    function instead of class

--- codey-catcher-app/src/components/KidsOrPetsSelect.js
+++ codey-catcher-app/src/components/KidsOrPetsSelect.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class KidsOrPetsSelect extends Component {
-    render() {
-        return (
-            <div id="kids-or-pets-wrap">
-                <label>Kids or Pets?</label>
-                <select id="dependents-select" name={this.props.name} onChange={this.props.onChange}>
-                    <option value="none" ></option>
-                    <option value="kids" >Kids</option>
-                    <option value="pets" >Pets</option>
-                </select>
-            </div>
-        )
-    }
+export default function KidsOrPetsSelect(props){
+   
+    return (
+        <div id="kids-or-pets-wrap">
+            <label>Kids or Pets?</label>
+            <select id="dependents-select" name={props.name} onChange={props.onChange}>
+                <option value="none" ></option>
+                <option value="kids" >Kids</option>
+                <option value="pets" >Pets</option>
+            </select>
+        </div>
+    )
+  
 }
