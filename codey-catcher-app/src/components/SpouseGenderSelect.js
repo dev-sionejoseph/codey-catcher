d01269132a42e158e23b2b059a2a0f6493b2af63
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-25 20:36:43 -0500

    function instead of class pt 2

--- codey-catcher-app/src/components/SpouseGenderSelect.js
+++ codey-catcher-app/src/components/SpouseGenderSelect.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class SpouseGenderSelect extends Component{
-    render() {
-        return (
-            <div id="spouse-gender-wrap">
-                <label>Spouse Gender?</label>
-                <select id="gender-select" name={this.props.name} onChange={this.props.onChange}>
-                    <option value="none"></option>
-                    <option value="man">Man</option>
-                    <option value="woman">Woman</option>
-                    <option value="nonbinary">Nonbinary</option>
-                </select>
-            </div>
-        )
-    }
+export default function SpouseGenderSelect(props) {
+  
+    return (
+        <div id="spouse-gender-wrap">
+            <label>Spouse Gender?</label>
+            <select id="gender-select" name={props.name} onChange={props.onChange}>
+                <option value="none"></option>
+                <option value="man">Man</option>
+                <option value="woman">Woman</option>
+                <option value="nonbinary">Nonbinary</option>
+            </select>
+        </div>
+    )
+
 }
