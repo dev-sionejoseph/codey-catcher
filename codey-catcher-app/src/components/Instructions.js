d38752dd15c1cfcddded03836184988149ea8d00
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-26 18:46:22 -0500

    more styling

--- codey-catcher-app/src/components/Instructions.js
+++ codey-catcher-app/src/components/Instructions.js
@@ -8,14 +8,14 @@ export default function Instructions() {
                 <div id="inner-instructions-wrap">
                     <h3>Instructions</h3>
                     <ul>
-                        <label>Divinations: </label>
+                        <p className="instruction-titles">Divinations: </p>
                         <li>Codey will predict who you will marry, 
                             where you will live, how many kids or pets, and your means of transportation
                         </li>
-                        <label>Ask Codey: </label>
+                        <p className="instruction-titles">Ask Codey: </p>
                         <li> Have a yes or no question? Ask Codey and she'll tell you what's what.
                         </li>
-                        <label>Love or Loss: </label>
+                        <p className="instruction-titles">Love or Loss: </p>
                         <li> Want to test the match between you and your beloved? 
                             Enter both your names in the input fields provided and Codey will calculate how likely it is to succeed.
                         </li>
