b01a1dd3475240e2d1e89b6cacc1b5b68bb6a58e
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-25 20:37:11 -0500

    function instead of class pt 3

--- codey-catcher-app/src/components/NameInput.js
+++ codey-catcher-app/src/components/NameInput.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class NameInput extends Component {
+export default function NameInput(props) {
 
-    render() {
-        return (
-            <div>
-                <label>Your First Name:</label>
-                <input type="text" id="fname-input" name="fname" onChange={this.props.onChange}></input>
-                <label>Partner's First Name</label>
-                <input type="text" id="sname-input" name="sname" onChange={this.props.onChange}></input>
-                <button id="compatibility-button" onClick={this.props.onClick}>Love or Loss?</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <label>Your First Name:</label>
+            <input type="text" id="fname-input" name="fname" onChange={props.onChange}></input>
+            <label>Partner's First Name</label>
+            <input type="text" id="sname-input" name="sname" onChange={props.onChange}></input>
+            <button id="compatibility-button" onClick={props.onClick}>Love or Loss?</button>
+        </div>
+    )
+    
 }
